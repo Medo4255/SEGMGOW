f40c11a3bc032012262e9c4d21795665269ce62a
Ahmed Hamdy <162977062+Medo4255@users.noreply.github.com>
2024-05-09 12:22:31 +0300

    added admin and login page

--- js/loginAuthentication.js
+++ js/loginAuthentication.js
@@ -1,13 +1,34 @@
 
 var loginBtn = document.getElementById("login_btn"); 
 
+let adminNames = ["admin"];
+let adminPasswords = ["admin123"];
+
+let donorNames = ["hamdy","aboelela","hegawy","hema","sameh"];
+let donorPasswords = ["hamdy123","aboelela123","hegawy123","hema123","sameh123"];
+
+let orgNames = ["org1","org2","org3"];
+let orgPasswords = ["organization123","organization234","organization345"];
+
 loginBtn.addEventListener("click", function() {
     var username = document.getElementsByName("username")[0].value;
     var password = document.getElementsByName("password")[0].value;
 
-    if (username === "admin" && password === "admin123") {
-        window.location.replace("admin.html");
-    } else {
-        window.location.replace("about.html");
+    for(let i = 0; i<adminNames.length;i++){
+        if(username == adminNames[i] && password == adminPasswords[i]){
+            window.location.replace("admin.html");
+        }
+    }
+
+    for(let i = 0; i<donorNames.length;i++){
+        if(username == donorNames[i] && password == donorPasswords[i]){
+            window.location.replace("donor.html");
+        }
+    }
+
+    for(let i = 0; i<orgNames.length;i++){
+        if(username == orgNames[i] && password == orgPasswords[i]){
+            window.location.replace("org.html");
+        }
     }
 });
