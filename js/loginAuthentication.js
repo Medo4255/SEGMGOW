dd4a43df06f2df44742e94a51626e9e989b43d60
Ahmed Hamdy <162977062+Medo4255@users.noreply.github.com>
2024-05-11 16:29:26 +0300

    changes

--- js/loginAuthentication.js
+++ js/loginAuthentication.js
@@ -11,8 +11,8 @@ let orgNames = ["org1","org2","org3"];
 let orgPasswords = ["organization123","organization234","organization345"];
 
 loginBtn.addEventListener("click", function() {
-    var username = document.getElementsByName("username")[0].value;
-    var password = document.getElementsByName("password")[0].value;
+    var username = document.getElementsById("username")[0].value;
+    var password = document.getElementsById("password")[0].value;
 
     for(let i = 0; i<adminNames.length;i++){
         if(username == adminNames[i] && password == adminPasswords[i]){
