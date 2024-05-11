af00c883d80f32f7bb4bcd2d1b27961f4e3bbe34
Ahmed Hamdy <162977062+Medo4255@users.noreply.github.com>
2024-05-11 16:56:44 +0300

    reorganized files

--- js/loginAuthentication.js
+++ js/loginAuthentication.js
@@ -1,34 +1,34 @@
-
-var loginBtn = document.getElementById("login_btn"); 
-
-let adminNames = ["admin"];
-let adminPasswords = ["admin123"];
-
-let donorNames = ["hamdy","aboelela","hegawy","hema","sameh"];
-let donorPasswords = ["hamdy123","aboelela123","hegawy123","hema123","sameh123"];
-
-let orgNames = ["org1","org2","org3"];
-let orgPasswords = ["organization123","organization234","organization345"];
-
-loginBtn.addEventListener("click", function() {
-    var username = document.getElementsById("username")[0].value;
-    var password = document.getElementsById("password")[0].value;
-
-    for(let i = 0; i<adminNames.length;i++){
-        if(username == adminNames[i] && password == adminPasswords[i]){
-            window.location.replace("admin.html");
-        }
-    }
-
-    for(let i = 0; i<donorNames.length;i++){
-        if(username == donorNames[i] && password == donorPasswords[i]){
-            window.location.replace("donor.html");
-        }
-    }
-
-    for(let i = 0; i<orgNames.length;i++){
-        if(username == orgNames[i] && password == orgPasswords[i]){
-            window.location.replace("org.html");
-        }
-    }
-});
+var loginBtn = document.getElementById("login-btn"); 
+    
+  let adminNames = ["admin"];
+  let adminPasswords = ["admin123"];
+  
+  let donorNames = ["hamdy","aboelela","hegawy","hema","sameh"];
+  let donorPasswords = ["hamdy123","aboelela123","hegawy123","hema123","sameh123"];
+  
+  let orgNames = ["org1","org2","org3"];
+  let orgPasswords = ["organization123","organization234","organization345"];
+  
+  loginBtn.addEventListener("click", function() {
+      var username = document.getElementById("username").value;
+      var password = document.getElementById("password").value;
+  
+      for(let i = 0; i<adminNames.length;i++){
+          if(username == adminNames[i] && password == adminPasswords[i]){
+            console.log("wocwiedcuiwcbkwbckws");
+            window.location.replace("../html/admin.html");
+          }
+      }
+  
+      for(let i = 0; i<donorNames.length;i++){
+          if(username == donorNames[i] && password == donorPasswords[i]){
+            window.location.replace("../html/donor.html");
+          }
+      }
+  
+      for(let i = 0; i<orgNames.length;i++){
+          if(username == orgNames[i] && password == orgPasswords[i]){
+            window.location.replace("../html/org.html");
+          }
+      }
+  });
